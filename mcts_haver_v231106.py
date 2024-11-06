326b4149f7029318120fba2b1f7c4376b05890d3
Tuan Nguyen <tnguyen9210@gmail.com>
2024-11-06 15:31:31 -0700

    update

--- mcts_haver_v231106.py
+++ mcts_haver_v231106.py
@@ -278,6 +278,33 @@ class MCTS:
             
         action = np.random.choice(best_actions)
         
+        # best_actions = [i if action_ucbs[i] == np.max(action_ucbs) for i in range(self.num_actions)]
+        # action = np.argmax(action_ucbs)
+    
+
+        # best_action = None
+        # max_ucb = float("-inf")
+        
+        # for a in range(self.num_actions):
+        #     a_nvisits = action_nvisits[a]
+        #     a_value = action_values[a]
+        #     if a_nvisits != 0:
+        #         bonus = math.sqrt(2*math.log(total_nvisits)/a_nvisits)
+        #         a_ucb = a_value + bonus*self.hparam_ucb_scale
+                
+        #     if a_ucb > max_ucb:
+        #         max_ucb = a_ucb
+        #         best_action = a
+
+        #     if debug is True:
+        #         logging.warning(f"action={a}")
+        #         logging.warning(f"total_nvisits={total_nvisits}")
+        #         logging.warning(f"action.value={a_value:0.4f}")
+        #         logging.warning(f"action.nvisits={a_nvisits}")
+        #         logging.warning(f"action.bonus={bonus:0.4f}")
+        #         logging.warning(f"action.ucb={action_ucb:0.4f}")
+        #         logging.warning(f"best_action={best_action},max_ucb={max_ucb:0.4f},")
+
         return action
 
     
@@ -311,6 +338,46 @@ class MCTS:
 def haver21count(
         action_values, action_nvisits, action_vars, hparam_haver_var, debug=False):
     
+    # num_actions = len(action_values)  
+    # num_actions_visited = np.sum(action_nvisits > 0)
+    # total_nvisits = np.sum(action_nvisits)
+
+    # visited_idxes = action_nvisits != 0
+
+    # # compute rhat
+    # gam_log = np.zeros(num_actions)
+    # action_gams = np.inf*np.ones(num_actions)
+    # gam_log[visited_idxes] = \
+    #     (num_actions_visited*total_nvisits/action_nvisits[visited_idxes])**4
+    # action_gams[visited_idxes] = \
+    #     hparam_haver_var*np.sqrt(18/action_nvisits[visited_idxes]*np.log(gam_log[visited_idxes]))
+    # action_lcbs = action_values - action_gams
+    # rhat_idx = np.argmax(action_lcbs)
+    # rhat_gam = action_gams[rhat_idx]
+    # max_lcb = action_lcbs[rhat_idx]
+
+    # Bset_muhats = np.zeros(num_actions)
+    # Bset_nvisits = np.zeros(num_actions)
+
+    # Bset_conds = visited_idxes & (action_values >= max_lcb) & (action_gams <= 3.0/2*rhat_gam)
+    # Bset_muhats[Bset_conds] = action_values[Bset_conds]
+    # Bset_nvisits[Bset_conds] = action_nvisits[Bset_conds]
+    
+    # Bset_probs = Bset_nvisits/np.sum(Bset_nvisits)
+    # haver_est = np.dot(Bset_muhats, Bset_probs)
+
+    # if debug:
+    #     logging.warn(f"action.nvisits = {action_nvisits}")
+    #     logging.warn(f"action_values = {action_values}")
+    #     logging.warn(f"max_lcb = {max_lcb:0.4f}")
+    #     # logging.warn(f"Bset_idxes = {Bset_idxes}")
+    #     logging.warn(f"Bset_conds = {Bset_conds}")
+    #     logging.warn(f"Bset_nvisits = {Bset_nvisits}")
+    #     logging.warn(f"Bset_probs = {Bset_probs}")
+    #     logging.warn(f"Bset_muhats = {Bset_muhats}")
+    #     # loging.warn(tmp)
+    #     logging.warn(f"haver_est = {haver_est:.2f}")
+
     num_actions = len(action_values)  
     num_actions_visited = np.sum(action_nvisits > 0)
     total_nvisits = np.sum(action_nvisits)
@@ -327,8 +394,7 @@ def haver21count(
     my_action_nvisits = action_nvisits[valid_idx]
     my_action_vars = action_vars[valid_idx]
 
-    # my_vars = np.maximum(my_action_vars, hparam_haver_var/my_action_nvisits)
-    my_vars = hparam_haver_var/my_action_nvisits
+    my_vars = np.maximum(my_action_vars, hparam_haver_var/my_action_nvisits)
     my_gams_log = np.log(my_action_nvisits*total_nvisits/0.05)
     my_gams = np.sqrt(my_vars)*np.sqrt(2/my_action_nvisits*my_gams_log)
 
@@ -344,6 +410,81 @@ def haver21count(
     my_Bset_probs = my_Bset_nvisits/np.sum(my_Bset_nvisits)
     haver_est = np.dot(my_Bset_values, my_Bset_probs)
     
+    
+    # num_actions = len(action_values)  
+    # num_actions_visited = np.sum(action_nvisits > 0)
+    # total_nvisits = np.sum(action_nvisits)
+    
+    # visited_idxes = action_nvisits != 0
+    # action_vs = -10*np.ones(num_actions)
+    # action_vs[visited_idxes] = np.maximum(
+    #     action_vars[visited_idxes], hparam_haver_var/action_nvisits[visited_idxes])
+    
+    # rhat_idx = None
+    # rhat_gam = None
+    # rhat_muhat = None
+    # rhat_nvisits = None
+    # max_lcb = -np.inf
+    # for a in range(num_actions):
+    #     a_nvisits = action_nvisits[a]
+    #     a_value = action_values[a]
+    #     if a_nvisits != 0:
+    #         a_var = max(action_vars[a], hparam_haver_var/a_nvisits)
+    #         # a_var = action_vars[a] if action_vars[a] != 0 else hparam_haver_var
+            
+    #         gam_log = (num_actions_visited*total_nvisits/a_nvisits)**4
+    #         a_gam = np.sqrt(a_var)*np.sqrt(18/a_nvisits*np.log(gam_log))
+    #         # a_gam = hparam_haver_var*np.sqrt(18/a_nvisits*np.log(gam_log))
+            
+    #         a_lcb = a_value - a_gam
+    #         if a_lcb > max_lcb:
+    #             max_lcb = a_lcb
+    #             rhat_idx = a
+    #             rhat_gam = a_gam
+    #             rhat_nvisits = a_nvisits
+
+    # # print(max_lcb)
+    # Bset_idxes = []
+    # Bset_muhats = np.zeros(num_actions)
+    # Bset_nvisits = np.zeros(num_actions)
+    
+    # for a in range(num_actions):
+    #     a_nvisits = action_nvisits[a]
+    #     a_value = action_values[a]
+    #     if a_nvisits != 0:
+    #         a_var = max(action_vars[a], hparam_haver_var/a_nvisits)
+    #         # a_var = action_vars[a] if action_vars[a] != 0 else hparam_haver_var
+    #         # a_var = action_vars[a]
+            
+    #         gam_log = (num_actions_visited*total_nvisits/a_nvisits)**4
+    #         a_gam = np.sqrt(a_var)*np.sqrt(18/a_nvisits*np.log(gam_log))
+    #         # a_gam = hparam_haver_var*np.sqrt(18/a_nvisits*np.log(gam_log))
+
+    #         # if a_value >= max_lcb and a_gam <= 3.0/2*rhat_gam:
+    #         if a_value >= max_lcb and a_nvisits >= 4.0/9*rhat_nvisits:
+    #         # if a_value >= max_lcb:
+    #             Bset_muhats[a] = a_value
+    #             Bset_nvisits[a] = a_nvisits
+    #             # Bset_nvisits[a] = a_nvisits
+    #             Bset_idxes.append(a)
+
+    # Bset_probs = Bset_nvisits/np.sum(Bset_nvisits)
+    # haver_est = np.dot(Bset_muhats, Bset_probs)
+
+    # if debug:
+    #     logging.info(f"action.nvisits = {action_nvisits}")
+    #     logging.info(f"action_values = {action_values}")
+    #     logging.info(f"action_vars = {action_vars}")
+    #     logging.info(f"action_vs = {action_vs}")
+    #     logging.info(f"max_lcb = {max_lcb:0.4f}")
+    #     # logging.warn(f"Bset_idxes = {Bset_idxes}")
+    #     logging.info(f"Bset_nvisits = {Bset_nvisits}")
+    #     logging.info(f"Bset_probs = {Bset_probs}")
+    #     logging.info(f"Bset_muhats = {Bset_muhats}")
+    #     # loging.warn(tmp)
+    #     logging.info(f"haver_est = {haver_est:.2f}")
+    
+
     return haver_est
     
 
