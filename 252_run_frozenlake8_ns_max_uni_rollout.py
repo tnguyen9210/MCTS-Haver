a878a64a94e08cd940ef8a156a50a1ca2997b54a
Tuan Nguyen <tnguyen9210@gmail.com>
2024-12-11 11:07:04 -0700

    update

--- 252_run_frozenlake8_ns_max_uni_rollout.py
+++ 252_run_frozenlake8_ns_max_uni_rollout.py
@@ -40,7 +40,7 @@ args["rollout_method"] = ""
 args["render_mode"] = ""
 args["action_multi"] = 1
 args["ep_max_steps"] = 40
-args["mcts_rollout_max_depth"] = 1000
+args["mcts_rollout_max_depth"] = 2000
 print(f"num_trials = {args['num_trials']}")
 
 m = args["num_trials"]
