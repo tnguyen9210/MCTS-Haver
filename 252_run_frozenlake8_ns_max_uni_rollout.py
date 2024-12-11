eff30ef6c6aec05869c94d9875b2fabb5d73619b
Tuan Nguyen <tnguyen9210@gmail.com>
2024-12-11 11:38:16 -0700

    update

--- 252_run_frozenlake8_ns_max_uni_rollout.py
+++ 252_run_frozenlake8_ns_max_uni_rollout.py
@@ -95,7 +95,7 @@ hparam_ucb_scale_list = [np.sqrt(100)**(i/2) for i in range(2,8)]
 
 # num_trajectories_list = [200, 400, 100, 600, 800, 1000]
 # num_trajectories_list = [400, 600, 800]
-num_trajectories_list = [int(np.sqrt(100)**(i/2)) for i in range(9,10)]
+num_trajectories_list = [int(np.sqrt(100)**(i/2)) for i in range(5,7)]
 
 best_param_list = []
 max_reward_mean_list = []
