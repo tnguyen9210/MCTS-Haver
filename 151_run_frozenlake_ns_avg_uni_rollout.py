92a0d6cc532cafc6da7299d0850810f007d47230
Tuan Nguyen <tnguyen9210@gmail.com>
2025-03-06 16:38:52 -0700

    update

--- 151_run_frozenlake_ns_avg_uni_rollout.py
+++ 151_run_frozenlake_ns_avg_uni_rollout.py
@@ -152,7 +152,7 @@ print(res_text1)
 print(res_text2)
 
 tmp = f"num_trials = {m} \n"
-with open("./results/51_frozenlake_ns_avg_uni_rollout_v1.txt", 'w+') as f:
+with open("./results/151_frozenlake_ns_avg_uni_rollout_v1.txt", 'w+') as f:
     f.write(tmp)
     f.write(log_text)
     f.write("\n")
