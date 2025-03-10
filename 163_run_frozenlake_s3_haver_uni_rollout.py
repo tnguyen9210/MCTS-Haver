b893fcc67b246d0c481e0fa1b59e510654ea1c11
Tuan Nguyen <tnguyen9210@gmail.com>
2025-03-10 15:16:48 -0700

    update

--- 163_run_frozenlake_s3_haver_uni_rollout.py
+++ 163_run_frozenlake_s3_haver_uni_rollout.py
@@ -94,10 +94,13 @@ def run_trial(i_trial, Q_vit, env_seed, simulator_seed, mcts_seed, args):
 # hparam_ucb_scale_list = [32, 64, 128, 256, 512, 1024]
 hparam_ucb_scale_list = [np.sqrt(100)**(i/2) for i in range(2,8)]
 args["hparam_ucb_scale"] = 64
+print(hparam_ucb_scale_list)
 
 # hparam_haver_std_list = np.arange(10, 100, 10)
 # hparam_haver_std_list = [0, 1/16, 1/8, 1/4, 1, 4, 8, 16]
 hparam_haver_std_list = [0] + [np.sqrt(100)**(i/2) for i in range(-3,3)]
+hparam_haver_std_list = [0] + [np.sqrt(100)**(i/2) for i in range(4,-1,-1)]
+print(hparam_haver_std_list)
 
 # num_trajectories_list = [200, 500, 1000, 1500, 2000, 2500, 3000]
 # num_trajectories_list = [400, 600, 800]
