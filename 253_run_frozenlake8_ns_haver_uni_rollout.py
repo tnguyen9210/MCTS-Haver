da94749620e9d40a8206652b9509d97e86bc5467
Tuan Nguyen <tnguyen9210@gmail.com>
2025-03-10 11:30:57 -0700

    update

--- 253_run_frozenlake8_ns_haver_uni_rollout.py
+++ 253_run_frozenlake8_ns_haver_uni_rollout.py
@@ -101,8 +101,10 @@ print(hparam_haver_std_list)
 
 # num_trajectories_list = [200, 500, 1000, 1500, 2000, 2500, 3000]
 # num_trajectories_list = [400, 600, 800]
+num_trajectories_list = [600]
+num_trajectories_list = [600]
 # num_trajectories_list = [int(np.sqrt(100)**(i/2)) for i in range(5,6)]
-num_trajectories_list = [int(np.sqrt(100)**(i/2)) for i in range(6,7)]
+# num_trajectories_list = [int(np.sqrt(100)**(i/2)) for i in range(6,7)]
 
 # num_trajectories_list = [2]
 
@@ -190,7 +192,7 @@ print(res_text1)
 print(res_text2)
 
 tmp = f"num_trials = {m} \n"
-with open("./results/253_frozenlake_ns_haver_uni_rollout_v1.txt", 'w+') as f:
+with open("./results/253_frozenlake_ns_haver_uni_rollout_v2.txt", 'w+') as f:
     f.write(tmp)
     f.write(log_text)
     f.write("\n")
