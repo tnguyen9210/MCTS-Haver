e18a9ee33cf8b96f804f7425450af2f8e161bc21
Tuan Nguyen <tnguyen9210@gmail.com>
2025-03-10 11:02:17 -0700

    update

--- 153_run_frozenlake_ns_haver_uni_rollout.py
+++ 153_run_frozenlake_ns_haver_uni_rollout.py
@@ -90,8 +90,9 @@ args["hparam_ucb_scale"] = 64
 
 # hparam_haver_std_list = np.arange(10, 100, 10)
 # hparam_haver_std_list = [0, 1/16, 1/8, 1/4, 1, 4, 8, 16]
-hparam_haver_std_list = [0] + [np.sqrt(100)**(i/2) for i in range(-3,3)]
-hparam_haver_std_list = [0] + [np.sqrt(49)**(i/2) for i in range(0,6)]
+hparam_haver_std_list = [0] + [np.sqrt(100)**(i/2) for i in range(4,1,-1)]
+print(hparam_haver_std_list)
+# hparam_haver_std_list = [0] + [np.sqrt(49)**(i/2) for i in range(0,6)]
 
 # num_trajectories_list = [200, 500, 1000, 1500, 2000, 2500, 3000]
 # num_trajectories_list = [400, 600, 800]
