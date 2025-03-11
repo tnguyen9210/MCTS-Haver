166ec34cffe1ef59849b36133150f1c4a15b5410
Tuan Nguyen <tnguyen9210@gmail.com>
2025-03-10 17:50:16 -0700

    update

--- 161_run_frozenlake_s3_avg_uni_rollout.py
+++ 161_run_frozenlake_s3_avg_uni_rollout.py
@@ -39,7 +39,9 @@ args["update_method"] = "avg"
 args["rollout_method"] = ""
 args["render_mode"] = ""
 args["action_multi"] = 1
+args["mcts_rollout_max_depth"] = 1000
 print(f"num_trials = {args['num_trials']}")
+print(f"mcts_rollout_max_depth = {args['mcts_rollout_max_depth']}")
 
 
 m = args["num_trials"]
@@ -165,7 +167,7 @@ print(res_text1)
 print(res_text2)
     
 tmp = f"num_trials = {m} \n"
-with open("./results/161_frozenlake_s3_avg_uni_rollout_v1.txt", 'w+') as f:
+with open("./results/161_frozenlake_s3_avg_uni_rollout_v2.txt", 'w+') as f:
     f.write(tmp)
     f.write(log_text)
     f.write("\n")
